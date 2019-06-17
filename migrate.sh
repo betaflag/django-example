3753ba30ead02dd179efccd386c1282c0719126e
betaflag <hello@betaflag.com>
2019-06-17 19:03:11 -0400

    add migration step

---
+++ migrate.sh
@@ -0,0 +1,7 @@
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+chmod +x cloud_sql_proxy
+./cloud_sql_proxy -instances=django-example-244011:us-east1:django-example=tcp:3306
+
+pip install -r requirements.txt
+python manage.py migrate
+
