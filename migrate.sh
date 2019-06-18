7c3c011b154660695f4f9d96f9601c0f212c854d
betaflag <hello@betaflag.com>
2019-06-17 20:00:23 -0400

    promote build step

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,5 @@
 pip install -r requirements.txt
+
 wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
 chmod +x cloud_sql_proxy
 ./cloud_sql_proxy -instances=django-example-244011:us-east1:django-example=tcp:3306 &
