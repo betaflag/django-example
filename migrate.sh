30320142be1f72b472f5113b13cbed1386bb93ec
betaflag <hello@betaflag.com>
2019-06-17 20:24:41 -0400

    use no input for automatic db migrations

--- migrate.sh
+++ migrate.sh
@@ -4,4 +4,4 @@ wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_pro
 chmod +x cloud_sql_proxy
 ./cloud_sql_proxy -instances=${CLOUDSQL_INSTANCE}=tcp:3306 &
 
-python manage.py migrate --noinput
+python manage.py migrate --no-input
